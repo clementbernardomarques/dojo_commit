fc081bdb8782a52e0770319e158d475b731498ef
clementbernardomarques <clementb@hokla.com>
2021-10-13 18:14:03 +0200

    add show me button that show or hide my list of food

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 export const Home = ( { firstMeal, secondMeal, thirdMeal }) => {
+    let [showList, updateShowList] = useState(false);
+    console.log(showList)
     return (
         <div>
             <h1>My Favourite Foods</h1>
-            <ul>
-                <li> { firstMeal } </li>
-                <li> { secondMeal } </li>
-                <li> { thirdMeal } </li>
-            </ul>
+            <button onClick={() => {updateShowList(!showList);}}>Show me</button>
+            <div style={{display: showList ? "" : "none"}}>
+                <ul>
+                    <li>{firstMeal}</li>
+                    <li>{secondMeal}</li>
+                    <li>{thirdMeal}</li>
+                </ul>
+            </div>
         </div>
     )
 }
