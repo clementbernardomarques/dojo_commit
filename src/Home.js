256b226f898c179cf6cf2a26ddff6ba854e00ac3
clementbernardomarques <clementb@hokla.com>
2021-10-13 17:53:39 +0200

    my First Page saying my favourite food

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>My Favourite Foods</h1>
+            <ul>
+                <li>Eggs</li>
+                <li>Pasta</li>
+                <li>Queijo Coalho</li>
+            </ul>
+        </div>
+    )
+}
